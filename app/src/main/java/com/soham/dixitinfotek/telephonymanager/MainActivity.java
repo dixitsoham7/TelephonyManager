cfb3c9980a30c894b41720ede85ba74cf7c8b028
dmkdfc <sohammalhar@gmail.com>
2018-10-01 09:29:10 +0530

    Initial commit

---
+++ app/src/main/java/com/soham/dixitinfotek/telephonymanager/MainActivity.java
@@ -0,0 +1,153 @@
+package com.soham.dixitinfotek.telephonymanager;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    TextView textView1;
+
+    TelephonyManager tm;
+    PhoneStateListener mPhoneStateListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView1 = (TextView) findViewById(R.id.textView1);
+
+        Button mDialButton = (Button) findViewById(R.id.btn_dial);
+        final EditText mPhoneNoEt = (EditText) findViewById(R.id.et_phone_no);
+
+        mDialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNo = mPhoneNoEt.getText().toString();
+                if(!TextUtils.isEmpty(phoneNo)) {
+                    String dial = "tel:" + phoneNo;
+                    startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+                }else {
+                    Toast.makeText(MainActivity.this, "Enter a phone number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        mPhoneStateListener = new PhoneStateListener() {
+            @Override
+            public void onCallStateChanged(int state, String incomingNumber) {
+                super.onCallStateChanged(state, incomingNumber);
+
+                switch (state) {
+                    case TelephonyManager.CALL_STATE_IDLE:
+                        Toast.makeText(MainActivity.this, "CALL_STATE_IDLE", Toast.LENGTH_SHORT).show();
+                        break;
+                    case TelephonyManager.CALL_STATE_RINGING:
+                        Toast.makeText(MainActivity.this, "CALL_STATE_RINGING", Toast.LENGTH_SHORT).show();
+                        break;
+                    case TelephonyManager.CALL_STATE_OFFHOOK:
+                        Toast.makeText(MainActivity.this, "CALL_STATE_OFFHOOK", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+
+        //Get the instance of TelephonyManager
+        tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+        //Calling the methods of TelephonyManager the returns the information
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        String IMEINumber = tm.getDeviceId();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        String subscriberID = tm.getDeviceId();
+        String SIMSerialNumber=tm.getSimSerialNumber();
+        String networkCountryISO=tm.getNetworkCountryIso();
+        String SIMCountryISO=tm.getSimCountryIso();
+        String softwareVersion=tm.getDeviceSoftwareVersion();
+        String voiceMailNumber=tm.getVoiceMailNumber();
+
+        //Get the phone type
+        String strphoneType="";
+
+        int phoneType=tm.getPhoneType();
+
+        switch (phoneType)
+        {
+            case (TelephonyManager.PHONE_TYPE_CDMA):
+                strphoneType="CDMA";
+                break;
+            case (TelephonyManager.PHONE_TYPE_GSM):
+                strphoneType="GSM";
+                break;
+            case (TelephonyManager.PHONE_TYPE_NONE):
+                strphoneType="NONE";
+                break;
+        }
+
+        //getting information if phone is in roaming
+        boolean isRoaming=tm.isNetworkRoaming();
+
+        String info="Phone Details:\n";
+        info+="\n IMEI Number:"+IMEINumber;
+        info+="\n SubscriberID:"+subscriberID;
+        info+="\n Sim Serial Number:"+SIMSerialNumber;
+        info+="\n Network Country ISO:"+networkCountryISO;
+        info+="\n SIM Country ISO:"+SIMCountryISO;
+        info+="\n Software Version:"+softwareVersion;
+        info+="\n Voice Mail Number:"+voiceMailNumber;
+        info+="\n Phone Network Type:"+strphoneType;
+        info+="\n In Roaming? :"+isRoaming;
+
+        Toast.makeText(MainActivity.this,info,Toast.LENGTH_SHORT).show();
+
+        textView1.setText(info);//displaying the information in the textView
+
+
+    }
+
+    // ...
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+    }
+// ...
+}
